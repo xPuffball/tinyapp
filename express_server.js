45cfb130c683b596bc79a4bec65f7e2eefacf14f
Jaemin Han <j4emin.han@gmail.com>
2021-05-12 03:27:26 +0000

    created barebones ejs page for registration

--- express_server.js
+++ express_server.js
@@ -81,6 +81,10 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 })
 
+app.get("/register", (req, res) => {
+  res.render("urls_register", {username: req.cookies.username})
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
